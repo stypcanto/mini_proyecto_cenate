6368dc8d190bf25ef78d0a94424f5065a6f9b4f5
EPY <epy@gmail.com>
2026-02-03 17:50:47 -0500

    Cambios aceptados

--- backend/src/main/java/com/styp/cenate/api/bolsas/SolicitudBolsaController.java
+++ backend/src/main/java/com/styp/cenate/api/bolsas/SolicitudBolsaController.java
@@ -17,7 +17,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.time.LocalDate;
